1a0394b3c01450c5a6086153be44f770de81a256
galip <galipts1@gmail.com>
2023-07-02 22:59:12 +0300

    v5

--- lesson-06/build/js/main.js
+++ lesson-06/build/js/main.js
@@ -1,6 +1,6 @@
 "use strict";
 class Coder {
-    constructor(name, music, age, lang) {
+    constructor(name, music, age, lang = "typescript") {
         this.name = name;
         this.music = music;
         this.age = age;
@@ -10,5 +10,37 @@ class Coder {
         this.age = age;
         this.lang = lang;
     }
+    getAge() {
+        return `hello, I'm ${this.age}`;
+    }
+    getName() {
+        if (this.name)
+            `hello, I m ${this.name}`;
+        return "no name";
+    }
 }
-const victor = new Coder("ali", "rock", 44, "typescript");
+const victor = new Coder("ali", "rock", 25);
+console.log(victor.getAge());
+console.log(victor.getName());
+///console.log(victor.age);
+class webDev extends Coder {
+    constructor(computer, name, music, age) {
+        super(name, music, age);
+        this.computer = computer;
+        this.getLang = () => {
+            return ` I write ${this.lang}`;
+        };
+        this.computer = computer;
+    }
+}
+const Sara = new webDev("mac", "sara", "lofi", 25);
+console.log(Sara.getLang());
+class Guitarist {
+    constructor(name, instrument) {
+        this.name = name;
+        this.instrument = instrument;
+    }
+    play(action) {
+        return ` ${this.name} ${action} the ${this.instrument}`;
+    }
+}
--- lesson-06/src/main.ts
+++ lesson-06/src/main.ts
@@ -1,15 +1,65 @@
 class Coder {
+  secondLang!: string;
+
   constructor(
     public readonly name: string,
     public music: string,
     private age: number,
-    protected lang: string
+    protected lang: string = "typescript"
   ) {
     this.name = name;
     this.music = music;
     this.age = age;
     this.lang = lang;
   }
+  public getAge() {
+    return `hello, I'm ${this.age}`;
+  }
+  public getName() {
+    if (this.name) `hello, I m ${this.name}`;
+    return "no name";
+  }
 }
 
-const victor = new Coder("ali", "rock", 44, "typescript");
+const victor = new Coder("ali", "rock", 25);
+console.log(victor.getAge());
+console.log(victor.getName());
+///console.log(victor.age);
+
+class webDev extends Coder {
+  constructor(
+    public computer: string,
+    name: string,
+    music: string,
+    age: number
+  ) {
+    super(name, music, age);
+    this.computer = computer;
+  }
+  public getLang = () => {
+    return ` I write ${this.lang}`;
+  };
+}
+const Sara = new webDev("mac", "sara", "lofi", 25);
+console.log(Sara.getLang());
+// console.log(Sara.age);
+// console.log(Sara.lang);
+/////////////////////////////////////////////
+
+interface Musician {
+  name: string;
+  instrument: string;
+  play(action: string): string;
+}
+
+class Guitarist implements Musician {
+  name: string;
+  instrument: string;
+  constructor(name: string, instrument: string) {
+    this.name = name;
+    this.instrument = instrument;
+  }
+  play(action: string) {
+    return ` ${this.name} ${action} the ${this.instrument}`;
+  }
+}
