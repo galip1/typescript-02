47a8488486d02843fa905fee685a6ceb05e4d57e
galip <galipts1@gmail.com>
2023-07-05 23:36:52 +0300

    v6

--- lesson-06/build/js/main.js
+++ lesson-06/build/js/main.js
@@ -44,3 +44,44 @@ class Guitarist {
         return ` ${this.name} ${action} the ${this.instrument}`;
     }
 }
+const Page = new Guitarist("jimmy", "guitar");
+console.log(Page.play("strums"));
+/////////////////////////////////////////////
+class Peeps {
+    static getCount() {
+        return Peeps.count;
+    }
+    constructor(name) {
+        this.name = name;
+        this.name = name;
+        this.id = ++Peeps.count;
+    }
+}
+Peeps.count = 0;
+const john = new Peeps("john");
+const ali = new Peeps("ali");
+const veli = new Peeps("veli");
+console.log(Peeps.count);
+console.log(ali.id);
+////////////////////////////////
+class Bands {
+    constructor() {
+        this.dataState = [];
+    }
+    get data() {
+        return this.dataState;
+    }
+    set data(value) {
+        if (Array.isArray(value) && value.every((el) => typeof el === "string")) {
+            this.dataState = value;
+            return;
+        }
+        else
+            throw new Error("param is not an array of strings");
+    }
+}
+const MyBands = new Bands();
+MyBands.data = ["ali", "veli", "hasan"];
+console.log(MyBands.data);
+MyBands.data = [...MyBands.data, "zz top"];
+console.log(MyBands.data);
--- lesson-06/src/main.ts
+++ lesson-06/src/main.ts
@@ -63,3 +63,55 @@ class Guitarist implements Musician {
     return ` ${this.name} ${action} the ${this.instrument}`;
   }
 }
+
+const Page = new Guitarist("jimmy", "guitar");
+console.log(Page.play("strums"));
+/////////////////////////////////////////////
+
+class Peeps {
+  static count: number = 0;
+  static getCount(): number {
+    return Peeps.count;
+  }
+  public id: number;
+
+  constructor(public name: string) {
+    this.name = name;
+    this.id = ++Peeps.count;
+  }
+}
+
+const john = new Peeps("john");
+const ali = new Peeps("ali");
+const veli = new Peeps("veli");
+
+console.log(Peeps.count);
+console.log(ali.id);
+
+////////////////////////////////
+
+class Bands {
+  private dataState: string[];
+
+  constructor() {
+    this.dataState = [];
+  }
+
+  public get data(): string[] {
+    return this.dataState;
+  }
+
+  public set data(value: string[]) {
+    if (Array.isArray(value) && value.every((el) => typeof el === "string")) {
+      this.dataState = value;
+      return;
+    } else throw new Error("param is not an array of strings");
+  }
+}
+
+const MyBands = new Bands();
+MyBands.data = ["ali", "veli", "hasan"];
+console.log(MyBands.data);
+
+MyBands.data = [...MyBands.data, "zz top"];
+console.log(MyBands.data);
